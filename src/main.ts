cfedf4a9e8cce0553932a6d94d918f8819df9790
selincinal <selin.cinal@ghostthinker.de>
2026-06-17 15:56:35 +0200

    added tailwind and primeicons

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,36 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Material from '@primeuix/themes/Material'
+import 'primeicons/primeicons.css'
+
 
 import App from './App.vue'
+import { definePreset } from '@primeuix/themes'
+
 
 const app = createApp(App)
+const MyPreset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '{teal.50}',
+      100: '{teal.100}',
+      200: '{teal.200}',
+      300: '{teal.300}',
+      400: '{teal.400}',
+      500: '{teal.500}',
+      600: '{teal.600}',
+      700: '{teal.700}',
+      800: '{teal.800}',
+      900: '{teal.900}',
+      950: '{teal.950}'
+    }
+  }
+})
 
 app.use(createPinia())
 app.use(PrimeVue, {
   theme: {
-    preset: Material,
+    preset: MyPreset,
   },
 })
 app.mount('#app')
