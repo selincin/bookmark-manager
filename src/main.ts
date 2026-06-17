88d43d3afaac3c494928166be71f3e71eb1d8bcb
selincinal <selin.cinal@ghostthinker.de>
2026-06-17 18:05:28 +0200

    added vue use, theme adjustments

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Material from '@primeuix/themes/Material'
+import Aura from '@primeuix/themes/Aura'
 import 'primeicons/primeicons.css'
 
 
@@ -12,7 +12,7 @@ import { definePreset } from '@primeuix/themes'
 
 
 const app = createApp(App)
-const MyPreset = definePreset(Material, {
+const MyPreset = definePreset(Aura, {
   semantic: {
     primary: {
       50: '{teal.50}',
