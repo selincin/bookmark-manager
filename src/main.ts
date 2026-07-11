15aee82e02e07fe28d8ef216ea1ab037b6c4da3d
selincinal <selin.cinal@ghostthinker.de>
2026-07-11 15:56:59 +0200

    fixed import

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { definePreset } from '@primeuix/themes'
 
-import Aura from '@primeuix/themes/Aura'
+import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 import router from './router'
 
